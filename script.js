4649bb040c4303891216d97856f147325747ba7f
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-08-05 20:32:41 +0200

    add javascript

---
+++ script.js
@@ -0,0 +1,53 @@
+//canvas API
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const radius = 2;
+let isPressed = false;
+let lineWidth = 2;
+let color = "green";
+let x;
+let y;
+
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+  x = e.offsetX;
+  y = e.offsetY;
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+    x = x2;
+    y = y2;
+  }
+});
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = lineWidth * 4;
+  ctx.stroke();
+}
+
+drawCircle(100, 200);
+drawLine(100, 200, 300, 300);
